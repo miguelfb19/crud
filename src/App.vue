a591155d0243f8708b289415b8e21a08895a7726
miguelfb19 <154536425+miguelfb19@users.noreply.github.com>
2024-04-23 18:31:15 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!DOCTYPE <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>CRUD Universidad de Manizales</title>
+    </head>
+    <body>
+      <div class="center">
+        <principal-component></principal-component>
+        <router-view></router-view>
+      </div>
+    </body>
+  </html>
+</template>
+
+<script>
+import principalComponent from "@/components/principalComponent";
+
+export default {
+  name: "App",
+  components: {
+    principalComponent,
+  },
+};
+</script>
+
+<style>
+@import "@/assets/css/styles";
+</style>
